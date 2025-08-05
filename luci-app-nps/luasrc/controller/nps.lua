295424c16e88466799f19edbbc62d4a0631ba54f
D-Jy <duan@d-jy.net>
2025-08-05 17:16:09 +0800

    检查安装状态、显示当前版本号

--- luci-app-nps/luasrc/controller/nps.lua
+++ luci-app-nps/luasrc/controller/nps.lua
@@ -10,8 +10,22 @@ function index()
 end
 
 function act_status()
+	local fs = require "nixio.fs"
 	local e = {}
-	e.running = luci.sys.call("pgrep nps > /dev/null") == 0
+
+	if not fs.access("/usr/bin/nps") then
+		e.installed    = false
+		e.running      = false
+		e.version      = ""
+		e.core_version = ""
+	else
+		e.installed   = true
+		e.running      = (luci.sys.call("pgrep nps > /dev/null") == 0)
+		local ver_out  = luci.sys.exec("/usr/bin/nps -version 2>/dev/null")
+		e.version      = ver_out:match("Version:%s*([v%d%.%-]+)")      or ""
+		e.core_version = ver_out:match("Core version:%s*([v%d%.%-]+)") or ""
+	end
+
 	luci.http.prepare_content("application/json")
 	luci.http.write_json(e)
 end
